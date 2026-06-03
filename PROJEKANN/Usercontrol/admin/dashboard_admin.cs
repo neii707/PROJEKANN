e4216161eaafb618ae70a2414842558591e64206
neii707 <itzur.nei@gmail.com>
2026-06-03 19:08:43 +0700

    aku

--- PROJEKANN/Usercontrol/admin/dashboard_admin.cs
+++ PROJEKANN/Usercontrol/admin/dashboard_admin.cs
@@ -75,20 +75,7 @@ namespace PROJEKANN.Usercontrol
 
         private void button2_Click(object sender, EventArgs e)
         {
-            Form1 formUtama = this.FindForm() as Form1;
 
-            if (formUtama != null)
-            {
-                MessageBox.Show("Form ketemu");
-
-                var halaman = new PROJEKANN.Usercontrol.admin.kelola_akun();
-
-                MessageBox.Show("UserControl berhasil dibuat");
-
-                formUtama.TampilkanHalaman(halaman);
-
-                MessageBox.Show("TampilkanHalaman selesai");
-            }
         }
     }
 }
--- PROJEKANN/Usercontrol/login.cs
+++ PROJEKANN/Usercontrol/login.cs
@@ -76,16 +76,11 @@ namespace PROJEKANN.Usercontrol
                                     break;
 
                                 case "nelayan":
-                                    // 1. Bersihkan semua control lama yang menempel di Form1 agar tidak tumpang tindih
+
+                                    mainForm.TampilkanHalaman(new PROJEKANN.Usercontrol.DashboardNelayan(mainForm, username));
                                     mainForm.Controls.Clear();
-
-                                    // 2. Inisialisasi Dashboard Nelayan dengan membawa parameter username
                                     PROJEKANN.Usercontrol.DashboardNelayan nelayanDashboard = new PROJEKANN.Usercontrol.DashboardNelayan(mainForm, username);
-
-                                    // 3. ATUR DOCK KE FILL (Ini kunci utama supaya komponennya langsung muncul memenuhi form utama)
                                     nelayanDashboard.Dock = DockStyle.Fill;
-
-                                    // 4. Masukkan ke dalam mainForm dan paksa muncul ke paling depan
                                     mainForm.Controls.Add(nelayanDashboard);
                                     nelayanDashboard.BringToFront();
                                     break;
