68675ab083b401fee41db1c225a2102942d62a08
AZZUMARA WIKAN <wikanamarjehaazzumara@gmail.com>
2026-06-03 12:27:36 +0700

    p

--- PROJEKANN/Usercontrol/Distributor/dashboard_distributor.cs
+++ PROJEKANN/Usercontrol/Distributor/dashboard_distributor.cs
@@ -7,7 +7,6 @@ using System.Text;
 using System.Windows.Forms;
 using Npgsql;
 using PROJEKANN.database;
-using System.Data;
 
 namespace PROJEKANN.Usercontrol
 {
@@ -25,7 +24,46 @@ namespace PROJEKANN.Usercontrol
 
         private void dashboard_distributor_Load(object sender, EventArgs e)
         {
+            MuatTransaksiTerkini();
+        }
 
+        private void MuatTransaksiTerkini()
+        {
+            try
+            {
+                using (NpgsqlConnection conn =
+                    DBConnection.GetConnection())
+                {
+                    conn.Open();
+
+                    string query = @"
+                        SELECT *
+                        FROM view_dashboard_distributor
+                        LIMIT 10;
+                    ";
+
+                    using (NpgsqlCommand cmd =
+                        new NpgsqlCommand(query, conn))
+                    {
+                        using (NpgsqlDataAdapter da =
+                            new NpgsqlDataAdapter(cmd))
+                        {
+                            DataTable dt = new DataTable();
+
+                            da.Fill(dt);
+
+                            dgvDashboard.DataSource = dt;
+
+                            dgvDashboard.AutoSizeColumnsMode =
+                                DataGridViewAutoSizeColumnsMode.Fill;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
--- PROJEKANN/Usercontrol/dashboard_admin.Designer.cs
+++ PROJEKANN/Usercontrol/dashboard_admin.Designer.cs
@@ -57,7 +57,7 @@
             panel1.Name = "panel1";
             panel1.Size = new Size(908, 555);
             panel1.TabIndex = 0;
-            panel1.Paint += panel1_Paint;
+            //panel1.Paint += panel1_Paint;
             // 
             // label2
             // 
